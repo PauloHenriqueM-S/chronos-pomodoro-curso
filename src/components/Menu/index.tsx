2fa2a3478c45b52927e67f9901b51c47a950287d
Paulo Henrique <phttm0501@gmail.com>
2026-09-01 09:25:40 -0400

    fix Menu component: update theme state initialization and icon rendering for theme toggle

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { FileCogIcon, HomeIcon, ListClockIcon, SunIcon } from "lucide-react";
+import { FileCogIcon, HomeIcon, ListClockIcon, MoonIcon, SunIcon } from "lucide-react";
 
 import styles from './styles.module.css'
 import { useState, useEffect } from "react";
@@ -6,7 +6,16 @@ import { useState, useEffect } from "react";
 type AvailableThemes = 'dark' | 'light'
 
 export function Menu() {
-  const [theme, setTheme] = useState<AvailableThemes>('dark');
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  }
 
   function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
     e.preventDefault();
@@ -18,15 +27,15 @@ export function Menu() {
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
   }, [theme])
 
   return (
     <nav className={styles.menu}>
-      <h1>{theme}</h1>
       <a className={styles.menuLink} href="#" aria-label="Ir para a Home" title="Ir para a Home"><HomeIcon /></a>
       <a className={styles.menuLink} href="#" aria-label="Ver Histórico" title="Ver Histórico"><ListClockIcon /></a>
       <a className={styles.menuLink} href="#" aria-label="Configurar Tempos " title="Configurar Tempos "><FileCogIcon /></a>
-      <a className={styles.menuLink} href="#" aria-label="Mudar Tema" title="Mudar Tema" onClick={handleThemeChange}><SunIcon /></a>
+      <a className={styles.menuLink} href="#" aria-label="Mudar Tema" title="Mudar Tema" onClick={handleThemeChange}>{nextThemeIcon[theme]}</a>
     </nav>
   )
 }
