d40d2e1c1d25d58775829b45362725b3d9375e76
Paulo Henrique <phttm0501@gmail.com>
2026-09-01 07:20:19 -0400

    add theme switching functionality: implement dark/light mode toggle in Menu component and update theme styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,32 @@
 import { FileCogIcon, HomeIcon, ListClockIcon, SunIcon } from "lucide-react";
 
 import styles from './styles.module.css'
+import { useState, useEffect } from "react";
+
+type AvailableThemes = 'dark' | 'light'
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
+      return nextTheme
+    })
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme])
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#"><HomeIcon /></a>
-      <a className={styles.menuLink} href="#"><ListClockIcon /></a>
-      <a className={styles.menuLink} href="#"><FileCogIcon /></a>
-      <a className={styles.menuLink} href="#"><SunIcon /></a>
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} href="#" aria-label="Ir para a Home" title="Ir para a Home"><HomeIcon /></a>
+      <a className={styles.menuLink} href="#" aria-label="Ver Histórico" title="Ver Histórico"><ListClockIcon /></a>
+      <a className={styles.menuLink} href="#" aria-label="Configurar Tempos " title="Configurar Tempos "><FileCogIcon /></a>
+      <a className={styles.menuLink} href="#" aria-label="Mudar Tema" title="Mudar Tema" onClick={handleThemeChange}><SunIcon /></a>
     </nav>
   )
 }
