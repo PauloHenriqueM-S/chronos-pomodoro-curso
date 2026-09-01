69016331b535b33f946c406d91d08de1afebbef3
Paulo Henrique <phttm0501@gmail.com>
2026-09-01 02:52:38 -0400

    refactor Form component: replace with inline form structure and remove styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './styles/global.css'
 import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
-import { Form } from './components/Form'
 import { Footer } from './components/Footer'
 import { CountDown } from './components/CountDown'
 
@@ -20,7 +19,22 @@ export function App() {
         <CountDown />
       </Container>
       <Container>
-        <Form />
+        <form className='form' action="">
+          <div className='formRow'>
+            <label htmlFor="input">task</label>
+            <input id="input" type="text" />
+          </div>
+          <div className='formRow'>
+            <p>Próximo ciclo é de FUNCTION</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
       </Container>
       <Container>
         <Footer />
