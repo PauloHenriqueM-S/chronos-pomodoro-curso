ece756628f0ad31ab88db681ea1fbfcdaaaa04d8
Paulo Henrique <phttm0501@gmail.com>
2026-09-01 09:40:41 -0400

    refactor App component: simplify structure by integrating Home page and creating MainTemplate for layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
+import { Home } from './pages/Home'
+
 import './styles/theme.css'
 import './styles/global.css'
-import { Container } from './components/Container'
-import { Logo } from './components/Logo'
-import { Menu } from './components/Menu'
-import { Footer } from './components/Footer'
-import { CountDown } from './components/CountDown'
-import { MainForm } from './components/MainForm'
 
 export function App() {
   return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <MainForm />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
+    <Home />
   )
 }
 
