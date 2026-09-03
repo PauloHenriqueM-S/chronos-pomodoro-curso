bfa95c7aaff83d44cf8382702b81e1d4f75ad6fd
Paulo Henrique <phttm0501@gmail.com>
2026-09-03 10:16:16 -0400

    add MainForm component: implement task creation functionality with state management

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,12 +2,55 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function MainForm() {
+  const { setState } = useTaskContext()
+  const taskNameInput = useRef<HTMLInputElement>(null)
+
+  function handleCreateNewTask(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    if (taskNameInput.current === null) return
+
+    const taskName = taskNameInput.current.value.trim()
+
+    if (!taskName) {
+      alert('Informe o nome da tarefa')
+      return
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime'
+    }
+
+    const secondsRemaining = newTask.duration * 60
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+  }
+
   return (
-    <form className='form' action="">
+    <form onSubmit={handleCreateNewTask} className='form' action="">
       <div className='formRow'>
-        <DefaultInput id="task" type="text" labelText="task" placeholder='Digite algo' />
+        <DefaultInput id="task" type="text" labelText="task" placeholder='Digite algo' ref={taskNameInput} />
       </div>
       <div className='formRow'>
         <p>Próximo ciclo é de FUNCTION</p>
